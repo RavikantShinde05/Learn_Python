2cc611ec30c6a425f7a34c3a61592c788e875f4f
RevX05@ <117154752+RavikantShinde05@users.noreply.github.com>
2026-02-16 23:47:59 +0530

    Add files via upload

---
+++ Python_Basics/1_variables.py
@@ -0,0 +1,20 @@
+# VARIABLES :
+
+# variables this is the way to store the data in computer's memore
+# store name / String charaters
+person_name = "David"
+print(person_name)
+# store numbers like age or float like height
+person_age = 25
+person_height = 6.2
+print(person_age, person_height)
+
+# we can store the data in different types or forms:
+# which is categorized in :
+# 1. Primitive and Non-Primitive type
+
+# Python is an case sensitive language like the boolean works only when True or False
+# rather than using true/TRUE or FALSE/false is used
+
+education_status = True
+married_status = False  # don't use false/FALSE
