ecb8b1a276375d2b206edc14e6dd3ff5688f3c14
RevX05@ <117154752+RavikantShinde05@users.noreply.github.com>
2026-02-16 23:51:28 +0530

    Add files via upload

---
+++ Python_Basics/7_numberFunctions.py
@@ -0,0 +1,18 @@
+# MATH FUNCTIONS WITH NUMBERS:
+
+import math
+# These are some functions used working with number.
+
+print(round(5.7))  # output> 6
+print(abs(-5.7))  # output> 5.7
+
+# to use more functions to work with numbers in mathematical way
+# "import math" this module will allow us to use more different
+# functions in our python Code.
+# To use it:
+
+print(math.ceil(8.7))  # output> 9
+print(math.fabs(8))  # output> 8.0
+print(math.factorial(5))  # output> 120
+print(math.tan(1))  # output> 1.557407
+print(math.log(25))  # output> 3.218875
