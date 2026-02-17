b008c1d59c750842b855fa12e22237c63e1b947e
RevX05@ <117154752+RavikantShinde05@users.noreply.github.com>
2026-02-17 22:22:52 +0530

    Add files via upload

---
+++ Python_Basics/9_comparisonOperators.py
@@ -0,0 +1,30 @@
+# COMPARISON OPERATORS:
+
+# COMPARISON OPERATORS WITH NUMBERS:
+10 > 3  # True
+10 >= 3  # True
+10 < 3  # False
+10 <= 3  # False
+10 < 20  # True
+10 <= 20  # True
+10 == 10  # True
+10 == "10"  # False
+10 != "10"  # True
+10 != 20  # True
+10 == 20  # False
+
+# COMPARISON OPERATORS WITH STRINGS:
+
+"student" > "school"  # True
+"student" >= "school"  # True
+"student" < "school"  # False
+"student" <= "school"  # False
+"school" < "student"  # True
+"school" <= "student"  # True
+"school" == "school"  # True
+"student" == "school"  # False
+"student" != "school"  # True
+"student" == 7
+"student" != "10"  # True
+"school" != "student"  # True
+"school" == "student"  # False
